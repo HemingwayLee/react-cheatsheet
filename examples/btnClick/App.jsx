847cd6b4e038915a3a44a0de13b3a296d08280c3
LeeHemingway <coherence0815@gmail.com>
2021-01-24 12:59:45 +0900

    complet example

--- examples/btnClick/App.jsx
+++ examples/btnClick/App.jsx
@@ -9,11 +9,17 @@ class App extends React.Component {
     e.target.style.backgroundColor = '#FF0000';
   }
 
+  preventMe(e) {
+    e.preventDefault();
+    alert("Hello, " + e.target.innerText);
+  }
+
   render() {
     return (
       <div>
         <button onClick={e => this.alertMe(e)}>Click Me to alert</button>
         <p onClick={e => this.changeMyColor(e)}>I am p tag, Click Me and Change my color</p>
+        <a href="http://www.google.com.tw" onClick={e => this.preventMe(e)}>I am a tag, click Me, I will not go to google</a>
       </div>
     );
   }
