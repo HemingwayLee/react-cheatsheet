90c76fa34e868a9ab429f6cba072a3ce1714089b
LeeHemingway <coherence0815@gmail.com>
2021-12-07 10:19:11 +0900

    add comments

--- examples/node14/components/markdown/src/index.js
+++ examples/node14/components/markdown/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactMarkdown from 'react-markdown'
+// TODO: does not work for now
+// import ReactMarkdown from 'https://cdn.skypack.dev/react-markdown@7?dts'
 import App from './App.jsx';
 
 const input = "# Hello  \n* Test1  \n* Test2";
