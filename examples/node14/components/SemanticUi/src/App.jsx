5ad8ef270b6344467f9bad60953195cec76d7c96
LeeHemingway <coherence0815@gmail.com>
2022-03-25 09:28:12 +0900

    add button ui example

--- examples/node14/components/SemanticUi/src/App.jsx
+++ examples/node14/components/SemanticUi/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react'
 
 class Welcome extends React.Component {
   render() {
-    return <h1>Hello, {this.props.name}</h1>;
+    return <div>
+        <h1>Hello, {this.props.name}</h1>
+        <Button>Click Here</Button>
+      </div>;
   }
 }
 
