00d503d37f1755267987134616c343f4b7ab6aa0
LeeHemingway <coherence0815@gmail.com>
2022-05-01 15:20:05 +0900

    add markdown switcher

---
+++ examples/node14/components/SemanticUi/markdownSwitcher/src/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Tab } from 'semantic-ui-react'
+import ReactMarkdown from 'react-markdown'
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { 
+      theContent: "# Try me  \n* Test1  \n* Test2"
+    };
+    this.generateMarkdown = this.generateMarkdown.bind(this);
+    this.generateEditor = this.generateEditor.bind(this);
+    this.handleTextInput = this.handleTextInput.bind(this);
+  }
+
+  handleTextInput(event) {
+    this.setState({theContent: event.target.value});
+  }
+
+  generateEditor() {
+    return <div><h3>Drag and Drop Image</h3><textarea onChange={this.handleTextInput} style={{width : '100%'}} rows="10">{this.state.theContent}</textarea></div>
+  }
+
+  generateMarkdown() {
+    return <ReactMarkdown>{this.state.theContent}</ReactMarkdown>
+  }
+
+  render() {
+    const panes = [
+      {
+        menuItem: 'Edit', render: () => {
+          return this.generateEditor();
+        }
+      },
+      { 
+        menuItem: 'Result', render: () => {
+          return this.generateMarkdown();
+        }
+      },
+    ];
+
+    return <div>
+        <h1>Hello, {this.props.name}</h1>
+        <hr />
+        <Tab panes={panes} />
+      </div>;
+  }
+}
+
+export default Welcome;
