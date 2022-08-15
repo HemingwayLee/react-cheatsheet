66cfb7ac42703dea2193ea2a65f537724964bb46
LeeHemingway <coherence0815@gmail.com>
2022-08-15 17:00:53 +0900

    init srt reader

---
+++ node14/components/mui/cra/srtReader/src/App.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import MuiDrawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import { MainListItem } from './listItems';
+import Chart from './myChart';
+import RecursiveTreeView from './treeView';
+
+const Drawer = styled(MuiDrawer, {})(
+  ({}) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: 360,
+      boxSizing: 'border-box'
+    },
+  }),
+);
+
+const theme = createTheme();
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Box sx={{ display: 'flex' }}>
+          <Drawer variant="permanent">
+            <List component="nav">
+              <MainListItem />
+            </List>
+          </Drawer>
+          <Box
+            component="main"
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === 'light'
+                  ? theme.palette.grey[100]
+                  : theme.palette.grey[900],
+              flexGrow: 1,
+              height: '100vh',
+              overflow: 'auto',
+            }}
+          >
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+              <Grid container spacing={3}>
+                <Grid item xs={12} md={8} lg={9}>
+                  <Paper
+                    sx={{
+                      p: 2,
+                      display: 'flex',
+                      flexDirection: 'column',
+                      height: 240,
+                    }}
+                  >
+                    <Chart />
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} md={4} lg={3}>
+                  <Paper
+                    sx={{
+                      p: 2,
+                      display: 'flex',
+                      flexDirection: 'column',
+                      height: 240,
+                    }}
+                  >
+                    <Chart />
+                  </Paper>
+                </Grid>
+                <Grid item xs={12}>
+                  <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                    <RecursiveTreeView />
+                  </Paper>
+                </Grid>
+              </Grid>
+            </Container>
+          </Box>
+        </Box>
+      </ThemeProvider>
+    );
+  }
+}
+
+export default Dashboard;
---
+++ node14/components/mui/cra/srtReader/src/listItems.js
@@ -0,0 +1,71 @@
+import * as React from 'react';
+
+import Button from '@mui/material/Button';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { ResponsiveContainer } from 'recharts';
+
+const { default: srtParser2 } = require("srt-parser-2")
+const parser = new srtParser2()
+
+export function MainListItem() {
+  const [stateItems, setItemValues] = React.useState([]);
+  
+  const renderItems = (items) => (
+    <div>
+      { Array.isArray(items)
+        ? items.map(x => {
+          return (
+            <ListItemButton key={x.id}>
+              <ListItemText 
+                primaryTypographyProps={{ style: { whiteSpace: "normal" } }}
+                primary={x.text} 
+                secondary={ x.startTime + " -> " + x.endTime } 
+              />
+            </ListItemButton>
+          );
+        })
+        : null}
+    </div>
+  );
+  
+  async function ReadFile(file) {
+    return await file.text()
+  }
+  
+  function handleSrtData(data) {
+    var result = parser.fromSrt(data);
+    console.log(result);
+  
+    setItemValues(result);
+  }
+  
+  function handleClick({target}) {
+    const selectedFile = target.files[0];
+    // console.log(selectedFile);
+  
+    const promise = new Promise(resolve => {
+      const fileContent = ReadFile(selectedFile);
+      resolve(fileContent);
+    });
+  
+    promise.then(fileContent => {
+      const filename = selectedFile.name;
+      console.log(filename);
+  
+      handleSrtData(fileContent);
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <Button variant="contained" component="label" onChange={handleClick}>
+        Upload Srt File
+        <input type="file" accept=".srt" hidden />
+      </Button>
+      <ResponsiveContainer width="95%" height={400}>
+        { renderItems(stateItems) }
+      </ResponsiveContainer>
+    </React.Fragment>
+  );
+}
