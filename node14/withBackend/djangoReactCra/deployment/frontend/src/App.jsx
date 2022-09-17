d869a27f66cc1d9c467fef8db725b13a329e72f2
LeeHemingway <coherence0815@gmail.com>
2022-09-17 16:57:50 +0900

    init cra example

---
+++ node14/withBackend/djangoReactCra/deployment/frontend/src/App.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Tab, Button } from 'semantic-ui-react'
+
+class Welcome extends React.Component {
+  doInsert() {
+    fetch('/api/insert/', {
+      method: 'GET',
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      alert(myJson["result"]);
+    });
+  }
+
+  showPerson() {
+    fetch('/api/show1/', {
+      method: 'GET',
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      alert(myJson["result"]);
+    });
+  }
+
+  handleTab1() {
+    return (
+      <div>
+        <Tab.Pane>Tab 1 Content From a function</Tab.Pane>
+        <Button secondary onClick={this.doInsert}>Call backend to do insertion</Button>
+        <Button secondary onClick={this.showPerson}>Call backend to show</Button>
+      </div>
+    )
+  }
+
+  render() {
+    const panes = [
+      {
+        menuItem: 'Tab 1', render: () => {
+          return this.handleTab1();
+        }
+      },
+      { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+      { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+    ];
+
+    return <div>
+        <h1>Hello, {this.props.name}</h1>
+        <hr />
+        <Tab panes={panes} />
+      </div>;
+  }
+}
+
+export default Welcome;
