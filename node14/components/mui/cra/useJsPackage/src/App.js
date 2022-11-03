8864db7c46a35f0449bbc0529ebbda0d60c9913d
LeeHemingway <coherence0815@gmail.com>
2022-11-03 13:49:00 +0900

    update app

--- node14/components/mui/cra/useJsPackage/src/App.js
+++ node14/components/mui/cra/useJsPackage/src/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 
-class Welcome extends React.Component {
-  handleClick(e) {
+d3.select("body").append("span").text("Hello, world!");
+
+export default function Welcome(props) {
+  const handleClick = () => {
     alert("Hello mui");
   };
     
-  render() {
-    return (
-      <div>
-        <h1>Hello, {this.props.name}</h1>
-        <Button variant="contained" color="primary" onClick={() => {
-          alert('clicked mui');
-        }}>
-          inline onclick
-        </Button>
-        <Button variant="outlined" onClick={this.handleClick}>
-          handler onclick
-        </Button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Hello, {props.name}</h1>
+      <Button variant="contained" color="primary" onClick={() => {
+        alert('clicked mui');
+      }}>
+        inline onclick
+      </Button>
+      <Button variant="outlined" onClick={handleClick}>
+        handler onclick
+      </Button>
+    </div>
+  );
 }
-
-export default Welcome;
-
-
 
